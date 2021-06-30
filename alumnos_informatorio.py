9660ff62af751c41fcfc695fa9cc799efd837ea1
vilajp <vilajp@gmail.com>
2021-06-30 13:35:24 -0300

    Se agrego segundo modulo a alumnos informatorio

--- alumnos_informatorio.py
+++ alumnos_informatorio.py
@@ -18,7 +18,9 @@ def cargo_alumno(base=dict()):
         elif es_entero(dni):
             base[dni] = dict()
             base[dni]["nombre"]= input("\tIngrese nombre del alunmo: ")
-            base[dni]["localidad"] = input("\tIngrese localidad:")
+            base[dni]["localidad"] = input("\tIngrese localidad: ")
+            base[dni]["notasBD"] = input("\tIngrese Notas de Base de Datos separadas por comas: ").split(",")
+            base[dni]["notasPW"] = input("\tIngrese Notas de Programacion Web separadas por comas: ").split(",")
             print()
             continuo = " "
             while continuo.lower() not in ("sn"):
@@ -27,6 +29,27 @@ def cargo_alumno(base=dict()):
                     sigo = False
     return base
 
+def consulto_notas(base):
+    sigo = True
+    while sigo:
+        limpiar_pantalla()
+        opcion = hacer_menu("1 - Lista un alumno", "2 - Lista todos los Alumnos", "3 - Salir")
+        if opcion == 3:
+            sigo = False
+            continue
+        elif opcion == 1:
+            dni = input("\tIngrese Numero de Documento a listar: ")
+            if dni  not in base.keys():
+                print("\tEse alumno no se encuentra en la base verifique!!!!!")
+                input("\tPresione una tecla para continuar...")
+                continue
+            else:
+                print(f"\tListado de notas para {base[dni]['nombre']}")
+                print(f"\tNotas de Base de Datos")
+                for nota in base[dni]["notasBD"]:
+                    print("\tNota: ", nota )
+                input()
+
 
 if __name__ == "__main__":
     sigo = True
@@ -34,8 +57,8 @@ if __name__ == "__main__":
     while sigo:
         limpiar_pantalla()
         opcion = hacer_menu("1 - Cargar Nuevo Alumno", "2 - Consultar Notas Alumnos", 
-        "3 - Ranking Promedios", "4 - Alumnos Aprobados", "5 - Salir")
-        if opcion == 5:
+        "3 - Ranking Promedios", "4 - Alumnos Aprobados","5 - Actualizar Alumnos", "6 - Salir")
+        if opcion == 6:
             sigo = False
             continue
         elif opcion == 1:
@@ -47,14 +70,26 @@ if __name__ == "__main__":
             print(base_alumnos.items())
             input()
         elif opcion == 2:
-            limpiar_pantalla()
-            consulto_notas(base_alumnos)
+            
+            if base_alumnos:
+                limpiar_pantalla()
+                consulto_notas(base_alumnos)
         elif opcion == 3:
-            limpiar_pantalla()
-            ranking_promedios(base_alumnos)
+            
+            if base_alumnos:
+                limpiar_pantalla()
+                ranking_promedios(base_alumnos)
         elif opcion == 4:
-            limpiar_pantalla()
-            alumnos_aprobados(base_alumnos)
+
+            if base_alumnos:
+                limpiar_pantalla()
+                alumnos_aprobados(base_alumnos)
+        
+        elif opcion == 5:
+            if base_alumnos:
+                limpiar_pantalla()
+                actualizo_alumnos(base_alumnos)
+    
         
         
 
